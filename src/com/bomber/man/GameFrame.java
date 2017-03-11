715709b44ab52862f6153342ee0f9290a3768d4d
n3o2k7i8ch5 <iwanicki.daniel@gmail.com>
2017-03-12 00:03:47 +0100

    Hi, I'm Dan.

--- src/com/bomber/man/GameFrame.java
+++ src/com/bomber/man/GameFrame.java
@@ -5,10 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import static com.bomber.man.Main.ABS_MAP_SIZE;
 import static com.bomber.man.Main.RESOLUTION;
 import static com.bomber.man.Main.VISIB_MAP_SIZE;
+import static com.bomber.man.Object.direction.NULL;
 
 /**
  * Created by Kisiel on 08.03.2017.
@@ -17,12 +19,17 @@ public class GameFrame extends JPanel implements ActionListener {
 
     Player player;
 
+    static final int frame_time = 10;
+
     ArrayList<Bomb> bomb_list = new ArrayList<>();
     ArrayList<Object> tile_list = new ArrayList<>();
-    ArrayList<Wall> solid_list = new ArrayList<>();
+    ArrayList<Solid> solid_list = new ArrayList<>();
+    ArrayList<Explosion> explosion_list = new ArrayList<>();
+
     static Bomb[][] bombs = new Bomb[ABS_MAP_SIZE][ABS_MAP_SIZE];
     static Object[][] tiles = new Object[ABS_MAP_SIZE][ABS_MAP_SIZE];
-    static Wall[][] solids = new Wall[ABS_MAP_SIZE][ABS_MAP_SIZE];
+    static Solid[][] solids = new Solid[ABS_MAP_SIZE][ABS_MAP_SIZE];
+    static Explosion[][] explosions = new Explosion[ABS_MAP_SIZE][ABS_MAP_SIZE];
 
     int x_map_shift = 0;
     int y_map_shift = 0;
@@ -44,22 +51,26 @@ public class GameFrame extends JPanel implements ActionListener {
             for(int j = 0; j< ABS_MAP_SIZE; j++)
                 if((i+j)%2==0)
                     tile_list.add(new Object(this, i, j, Object.GRASS_PATH));
+                else
+                    tile_list.add(new Object(this, i, j, Object.GRASS_LIGHT_PATH));
 
-        addWall(3, 4);
-        addWall(3, 5);
-        addWall(3, 6);
-        addWall(3, 8); addWall(4, 8);
+
+        addSoftWall(3, 4);
+        addSoftWall(3, 5);
+        addSoftWall(3, 6);
+        addSoftWall(3, 8); addSoftWall(4, 8);
 
 
 
         setFocusable(true);
         addKeyListener(new KeyAdapt(player));
-        timer = new Timer(10, this::actionPerformed);
+        timer = new Timer(frame_time, this::actionPerformed);
         timer.setRepeats(true);
         timer.start();
 
     }
 
+    @Override
     public void paint(Graphics g)
     {
         super.paint(g);
@@ -68,8 +79,8 @@ public class GameFrame extends JPanel implements ActionListener {
         paintMap(g2d);
         player.draw(g2d);
 
-        for(int i=0; i<bomb_list.size(); i++)
-            bomb_list.get(i).draw(g2d);
+        for(Bomb bomb : bomb_list)
+            bomb.draw(g2d);
 
     }
 
@@ -78,8 +89,11 @@ public class GameFrame extends JPanel implements ActionListener {
         for(Object tile : tile_list)
             tile.draw(g2d);
 
-        for(Wall wall : solid_list)
-            wall.draw(g2d);
+        for(Solid solid : solid_list)
+            solid.draw(g2d);
+
+        for(Explosion explosion : explosion_list)
+            explosion.draw(g2d);
 
     }
 
@@ -95,19 +109,61 @@ public class GameFrame extends JPanel implements ActionListener {
         else
             player.update();
 
+        for (Iterator<Bomb> it = bomb_list.iterator(); it.hasNext(); ) {
+            Bomb bomb = it.next();
+            bomb.destruct_time -= frame_time;
+            if (bomb.destruct_time == 0) {
+                it.remove();
+                detonate(bomb);
+            }
+        }
+
+        for (Iterator<Explosion> it = explosion_list.iterator(); it.hasNext(); ) {
+            Explosion explosion = it.next();
+            explosion.life_time -= frame_time;
+            if (explosion.life_time == 0) {
+                it.remove();
+            }
+        }
+
         repaint();
     }
 
-    void addWall(int X, int Y){
-        Wall wall = new Wall(this, X, Y);
+    void addHardWall(int X, int Y){
+        Wall wall = new Wall(this, X, Y, false);
         solid_list.add(wall);
         solids[X][Y] = wall;
     }
 
-    void addBomb(int X, int Y){
-        Bomb bomb = new Bomb(this, X, Y);
+    void addSoftWall(int X, int Y){
+        Wall wall = new Wall(this, X, Y, true);
+        solid_list.add(wall);
+        solids[X][Y] = wall;
+    }
+
+    void removeSolid(Solid solid){
+        solid_list.remove(solid);
+        solids[solid.X][solid.Y] = null;
+    }
+
+    void addBomb(int X, int Y, int fire_length){
+        if(bombs[X][Y]!=null)
+            return;
+        
+        Bomb bomb = new Bomb(this, X, Y, fire_length);
         bomb_list.add(bomb);
         bombs[X][Y] = bomb;
     }
 
+    void detonate(Bomb bomb){
+        bomb_list.remove(bomb);
+        bombs[bomb.X][bomb.Y] = null;
+        addExplosion(bomb.X, bomb.Y, bomb.fire_length, NULL);
+    }
+
+    void addExplosion(int X, int Y, int fire_length, MovingObject.direction direction){
+        Explosion explosion = new Explosion(this, X, Y, fire_length, direction);
+        explosion_list.add(explosion);
+        explosions[X][Y] = explosion;
+    }
 }
